6f16da3dabcd425857cf857d69cb81f74c420f08
arthur <arthur.escriou@gmail.com>
2021-07-05 18:10:54 +0200

    :tada: worker

---
+++ worker/main.js
@@ -0,0 +1,56 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const randInt = (min, max) => Math.floor(Math.random() * (max - min)) + min
+let mult = false
+let add = false
+
+app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
+
+if (process.env.MULT)
+  app.post('/mult', (req, res) => {
+    if (mult) {
+      console.error('mult', 'Already working')
+      res.status(403).send('Already working')
+      return
+    }
+    mult = true
+    const { a, b } = req.body
+    console.log('mult', req.body)
+    const duration = randInt(3000, 12000)
+    setTimeout(() => {
+      mult = false
+      res.send(JSON.stringify({ res: a * b, duration }))
+    }, duration)
+  })
+
+if (process.env.ADD)
+  app.post('/add', (req, res) => {
+    if (add) {
+      console.error('add', 'Already working')
+      res.status(403).send('Already working')
+      return
+    }
+    add = true
+    const { a, b } = req.body
+    console.log('add', req.body)
+    const duration = randInt(3000, 7000)
+    setTimeout(() => {
+      add = false
+      res.send(JSON.stringify({ res: a + b, duration }))
+    }, duration)
+  })
+
+app.get('/', (req, res) => {
+  res.send('ready to work')
+})
+
+app.listen(port, () => {
+  console.log(`Worker listening at http://localhost:${port}`)
+})
