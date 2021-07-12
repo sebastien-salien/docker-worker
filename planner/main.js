edb34e257056a13b07758fa0b6b4c94a9f3b319f
sebastien <salien.sebastien@gmail.com>
2021-07-12 17:03:54 +0200

    Exercice 3

--- planner/main.js
+++ planner/main.js
@@ -5,11 +5,14 @@ const nbTasks = parseInt(process.env.TASKS) || 20
 const randInt = (min, max) => Math.floor(Math.random() * (max - min)) + min
 const taskType = () => (randInt(0, 2) ? 'mult' : 'add')
 const args = () => ({ a: randInt(0, 40), b: randInt(0, 40) })
-
+const port = process.env.PORT
 const generateTasks = i =>
   new Array(i).fill(1).map(_ => ({ type: taskType(), args: args() }))
 
-let workers = ['http://172.18.0.1:8080','http://172.18.0.1:8081']
+const CONST_ADD= ['http://172.18.0.1:8080','http://172.18.0.1:8081','http://172.18.0.1:8082','http://172.18.0.1:8083']
+const CONST_MULT= ['http://172.18.0.1:8084','http://172.18.0.1:8085','http://172.18.0.1:8086','http://172.18.0.1:8087']
+
+let workers = [...CONST_ADD,...CONST_MULT]
 let tasks = generateTasks(nbTasks)
 let taskToDo = nbTasks
 
@@ -47,7 +50,16 @@ const main = async () => {
   while (taskToDo > 0) {
     await wait(100)
     if (workers.length === 0 || tasks.length === 0) continue
-    sendTask(workers[0], tasks[0])
+    if(tasks.type == 'mult'){
+      const  mult_worker = workers.find(w => CONST_MULT.includes(w)) 
+      if(!mult_worker) continue 
+      sendTask(mult_worker, tasks[0])
+    }else{
+      const  mult_worker = workers.find(w => CONST_ADD.includes(w)) 
+      if(!mult_worker) continue 
+      sendTask(mult_worker, tasks[0])
+    }
+    
   }
 }
 
